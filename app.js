5d22fccffb96e1358a77403f64a7e526c57a61a3
Farhan Ansari <farhan.ansari1902@gmail.com>
2022-03-19 17:14:30 +0530

    Updates

--- app.js
+++ app.js
@@ -81,8 +81,12 @@ app.post('/home', function (req, res) {
     res.render('post', )
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-    console.log('listening on port 3000');
+app.listen(port, function() {
+    console.log('Server has started');
 });
 
